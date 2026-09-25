8e4bf572d7016883d9d20a258e2ccaad64657e86
Sajjad Ullah <sajjadsoplex@gmail.com>
2026-09-25 15:41:13 +0500

    Fix GitHub Pages base path

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
+
+  // GitHub Pages repository path
+  basePath: "/soplex-sentinel",
+
   trailingSlash: true,
 
   images: {
